7c82fd0656bd73a72736fea4e25a840c84762a6e
Amazed1970 <psychophen@gmail.com>
2019-08-29 11:39:11 -0700

    Add files via upload

---
+++ ventas/precache-manifest.f66a4ebc0bf02f0b2027ecbcfd1eeb81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f158c9fd35c4048f0c86eae8d5daff92",
+    "url": "http://eduardomacias.000webhostapp.com/ventas/static/media/logo.f158c9fd.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "http://eduardomacias.000webhostapp.com/ventas/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c5a4138c53f4d193c8c8",
+    "url": "http://eduardomacias.000webhostapp.com/ventas/static/js/main.c5a4138c.chunk.js"
+  },
+  {
+    "revision": "1b3020668eb18b7e0509",
+    "url": "http://eduardomacias.000webhostapp.com/ventas/static/js/1.1b302066.chunk.js"
+  },
+  {
+    "revision": "c5a4138c53f4d193c8c8",
+    "url": "http://eduardomacias.000webhostapp.com/ventas/static/css/main.818449e2.chunk.css"
+  },
+  {
+    "revision": "1b3020668eb18b7e0509",
+    "url": "http://eduardomacias.000webhostapp.com/ventas/static/css/1.a0dba18b.chunk.css"
+  },
+  {
+    "revision": "489232b15f6f246799cdf7d8aded3bd0",
+    "url": "http://eduardomacias.000webhostapp.com/ventas/index.html"
+  }
+];
